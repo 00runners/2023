e4e7b6568096fda3d46c4e329f9bae7577e3d0bf
Han-park <han-park@g.hongik.ac.kr>
2023-03-08 11:37:33 +0900

    wip deploy

--- src/component.js
+++ src/component.js
@@ -1,8 +1,17 @@
-export default[
-    {
-    "componentName": "NavBar",
-    "html": ""
-},
-{
-
-}]
+export default {
+  "navBar" :
+    ` <nav id="navbar-example2" class="navbar bg-light px-3 fixed-top" style="font-size: 0.8rem; border-bottom: 1px black solid;">
+    <a class="navbar-brand" href="#" style="color: black; font-size: 1rem"><b>00runners</b>&nbsp;(Work In Progress)</a>
+    <ul class="nav nav-pills">
+        <li class="nav-item">
+             <a class="nav-link" style="border-radius: 50px; color: black"href="#crew">멤버</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" style="border-radius: 50px; color: black"href="#requirement">게스트</a>
+         </li>
+        <li class="nav-item">
+            <a class="nav-link" style="border-radius: 50px; color: black"href="#faq">Docs</a>
+         </li>
+    </ul>
+    </nav>`
+}
