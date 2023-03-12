05e64e9c76b5785cbe9ab98c64b1b8efc70c542b
Han-park <han-park@g.hongik.ac.kr>
2023-03-12 22:15:44 +0900

    abc

--- src/component.js
+++ src/component.js
@@ -1,16 +1,16 @@
 export default {
   "navBar" :
-    ` <nav id="navbar-example2" class="navbar bg-light px-3 fixed-top" style="font-size: 0.8rem; border-bottom: 1px black solid;">
-    <a class="navbar-brand" href="#" style="color: black; font-size: 1rem"><b>00runners</b>&nbsp;(Work In Progress)</a>
+    ` <nav id="navbar-example2" class="navbar bg-dark px-3 fixed-top" style="font-size: 0.8rem; border-bottom: 1px black solid;">
+    <a class="navbar-brand" href="#" style="color: white; font-size: 1rem"><b>00runners</b>&nbsp;(beta)</a>
     <ul class="nav nav-pills">
         <li class="nav-item">
-             <a class="nav-link" style="border-radius: 50px; color: black"href="#crew">멤버</a>
+             <a class="nav-link" style="border-radius: 50px; color: white" href="#crew">멤버</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" style="border-radius: 50px; color: black"href="#requirement">게스트</a>
+            <a class="nav-link" style="border-radius: 50px; color: white" href="#requirement">게스트</a>
          </li>
         <li class="nav-item">
-            <a class="nav-link" style="border-radius: 50px; color: black"href="#faq">Docs</a>
+            <a class="nav-link" style="border-radius: 50px; color: white" href="#faq">Docs</a>
          </li>
     </ul>
     </nav>`
---
+++ src/user.js
@@ -0,0 +1,627 @@
+export default[
+    {"uid": 0, "name": "김용용", "instagram": "00runners", "badge": ["Crew Mascot"]},
+    {
+        "uid": 1,
+        "name": "박종한",
+        "instagram": "jonghan_future",
+        "badge": ["HQ", "Crew Leader"]
+      },
+      {
+        "uid": 2,
+        "name": "안정희",
+        "instagram": "hi_zuk0118",
+        "badge": ["HQ", "Design Team"]
+      },
+      {
+        "uid": 3,
+        "name": "오상훈",
+        "instagram": "arte_ohrok",
+        "badge": ["HQ", "Training Team"]
+      },
+      {
+        "uid": 4,
+        "name": "이동윤",
+        "instagram": "alabraskii",
+        "badge": ["HQ", "Design Team"]
+      },
+      {
+        "uid": 5,
+        "name": "정석호",
+        "instagram": "805__00",
+        "badge": ["HQ", "Design Team"]
+      },
+      {
+        "uid": 6,
+        "name": "정유진",
+        "instagram": "jjins_29",
+        "badge": ["HQ", "Community Team"]
+      },
+      {
+        "uid": 7,
+        "name": "최은주",
+        "instagram": "silverbead428",
+        "badge": ""
+      },
+      {
+        "uid": 8,
+        "name": "이호성",
+        "instagram": "nabee_hs",
+        "badge": ""
+      },
+      {
+        "uid": 9,
+        "name": "안서연",
+        "instagram": "cherryahn_",
+        "badge": ""
+      },
+      {
+        "uid": 10,
+        "name": "장재희",
+        "instagram": "1011_jahee",
+        "badge": ""
+      },
+      {
+        "uid": 11,
+        "name": "김수민",
+        "instagram": "tdbmin",
+        "badge": ""
+      },
+      {
+        "uid": 12,
+        "name": "박정민",
+        "instagram": "jm._00",
+        "badge": ""
+      },
+      {
+        "uid": 13,
+        "name": "정현도",
+        "instagram": "im.__.dodo",
+        "badge": ""
+      },
+      {
+        "uid": 14,
+        "name": "박예원",
+        "instagram": "ehswkd7",
+        "badge": ""
+      },
+      {
+        "uid": 15,
+        "name": "조예진",
+        "instagram": "soyyejin",
+        "badge": ""
+      },
+      {
+        "uid": 16,
+        "name": "김사랑",
+        "instagram": "4_lieee",
+        "badge": ""
+      },
+      {
+        "uid": 17,
+        "name": "조민지",
+        "instagram": "gaenziminzi",
+        "badge": ""
+      },
+      {
+        "uid": 18,
+        "name": "이로운",
+        "instagram": "rrhn_l",
+        "badge": ""
+      },
+      {
+        "uid": 19,
+        "name": "이태호",
+        "instagram": "taeho34",
+        "badge": ""
+      },
+      {
+        "uid": 20,
+        "name": "이서현",
+        "instagram": "nxxhs",
+        "badge": ""
+      },
+      {
+        "uid": 21,
+        "name": "이한결",
+        "instagram": "han_guri",
+        "badge": ""
+      },
+      {
+        "uid": 22,
+        "name": "김동현",
+        "instagram": "umbra_66",
+        "badge": ""
+      },
+      {
+        "uid": 23,
+        "name": "이동경",
+        "instagram": "tokyo_lee__",
+        "badge": ""
+      },
+      {
+        "uid": 24,
+        "name": "황연수",
+        "instagram": "postvalentinegirl215",
+        "badge": ""
+      },
+      {
+        "uid": 25,
+        "name": "윤경환",
+        "instagram": "hwan_memoria",
+        "badge": ""
+      },
+      {
+        "uid": 26,
+        "name": "김성윤",
+        "instagram": "s._.yo0on",
+        "badge": ""
+      },
+      {
+        "uid": 27,
+        "name": "이동준",
+        "instagram": "move_ju",
+        "badge": ""
+      },
+      {
+        "uid": 28,
+        "name": "정다현",
+        "instagram": "dhyun_00",
+        "badge": ""
+      },
+      {
+        "uid": 29,
+        "name": "양혜민",
+        "instagram": "ham_ni.ya",
+        "badge": ""
+      },
+      {
+        "uid": 30,
+        "name": "이예림",
+        "instagram": "yediiim",
+        "badge": ""
+      },
+      {
+        "uid": 31,
+        "name": "한호정",
+        "instagram": "hoxjeong",
+        "badge": ""
+      },
+      {
+        "uid": 32,
+        "name": "김민주",
+        "instagram": "grace7542",
+        "badge": ""
+      },
+      {
+        "uid": 33,
+        "name": "조병연",
+        "instagram": "byounceee",
+        "badge": ""
+      },
+      {
+        "uid": 34,
+        "name": "유민서",
+        "instagram": "mln._.seo",
+        "badge": ["Pacer"]
+      },
+      {
+        "uid": 35,
+        "name": "오현정",
+        "instagram": "ohjeong__",
+        "badge": ""
+      },
+      {
+        "uid": 36,
+        "name": "전은서",
+        "instagram": "oneusmo",
+        "badge": ""
+      },
+      {
+        "uid": 37,
+        "name": "한재연",
+        "instagram": "mme2so",
+        "badge": ""
+      },
+      {
+        "uid": 38,
+        "name": "강수빈",
+        "instagram": "rtnqls_",
+        "badge": ""
+      },
+      {
+        "uid": 39,
+        "name": "김영우",
+        "instagram": "_zerorain_",
+        "badge": ""
+      },
+      {
+        "uid": 40,
+        "name": "김가현",
+        "instagram": "gaxhyeon",
+        "badge": ""
+      },
+      {
+        "uid": 41,
+        "name": "박선우",
+        "instagram": "suncow_www",
+        "badge": ""
+      },
+      {
+        "uid": 42,
+        "name": "김용은",
+        "instagram": "a_nanas66",
+        "badge": ""
+      },
+      {
+        "uid": 43,
+        "name": "김소정",
+        "instagram": "sjung.__.k",
+        "badge": ""
+      },
+      {
+        "uid": 44,
+        "name": "김형태",
+        "instagram": "kim._.shape",
+        "badge": ""
+      },
+      {
+        "uid": 45,
+        "name": "최한결",
+        "instagram": "choehangyeol",
+        "badge": ""
+      },
+      {
+        "uid": 46,
+        "name": "김성진",
+        "instagram": "kmakeitj",
+        "badge": ""
+      },
+      {
+        "uid": 47,
+        "name": "허정윤",
+        "instagram": "castorpollukx",
+        "badge": ""
+      },
+      {
+        "uid": 48,
+        "name": "이가진",
+        "instagram": "havingly",
+        "badge": ""
+      },
+      {
+        "uid": 49,
+        "name": "이경진",
+        "instagram": "Jin.pf_",
+        "badge": ""
+      },
+      {
+        "uid": 50,
+        "name": "이나희",
+        "instagram": "inahyi8",
+        "badge": ""
+      },
+      {
+        "uid": 51,
+        "name": "김민석",
+        "instagram": "minseok_826",
+        "badge": ""
+      },
+      {
+        "uid": 52,
+        "name": "김현기",
+        "instagram": "hyeon_gi_one",
+        "badge": ""
+      },
+      {
+        "uid": 53,
+        "name": "김남우",
+        "instagram": "kimnamwoo_1021",
+        "badge": ""
+      },
+      {
+        "uid": 54,
+        "name": "김민서",
+        "instagram": "minssoya__a",
+        "badge": ""
+      },
+      {
+        "uid": 55,
+        "name": "김진희",
+        "instagram": "wonderfullamp814",
+        "badge": ""
+      },
+      {
+        "uid": 56,
+        "name": "송유진",
+        "instagram": "s.uzin",
+        "badge": ""
+      },
+      {
+        "uid": 57,
+        "name": "안유나",
+        "instagram": "dbxoddl77",
+        "badge": ""
+      },
+      {
+        "uid": 58,
+        "name": "백인혁",
+        "instagram": "in_backia",
+        "badge": ""
+      },
+      {
+        "uid": 59,
+        "name": "정아람",
+        "instagram": "mini_on_rama",
+        "badge": ""
+      },
+      {
+        "uid": 60,
+        "name": "윤국진",
+        "instagram": "gukjiin",
+        "badge": ""
+      },
+      {
+        "uid": 61,
+        "name": "정명훈",
+        "instagram": "meeeeeng_hun",
+        "badge": ""
+      },
+      {
+        "uid": 62,
+        "name": "이민욱",
+        "instagram": "minuk_7",
+        "badge": ""
+      },
+      {
+        "uid": 63,
+        "name": "송예진",
+        "instagram": "br.__ghtrue",
+        "badge": ""
+      },
+      {
+        "uid": 64,
+        "name": "박강찬",
+        "instagram": "chanthebob",
+        "badge": ""
+      },
+      {
+        "uid": 65,
+        "name": "제갈원",
+        "instagram": "won_jegal",
+        "badge": ""
+      },
+      {
+        "uid": 66,
+        "name": "윤수빈",
+        "instagram": "soob_soop",
+        "badge": ""
+      },
+      {
+        "uid": 67,
+        "name": "이윤성",
+        "instagram": "leeyoun929",
+        "badge": ""
+      },
+      {
+        "uid": 68,
+        "name": "양주동",
+        "instagram": "j_judong",
+        "badge": ""
+      },
+      {
+        "uid": 69,
+        "name": "김수연",
+        "instagram": "jolly.suyeon",
+        "badge": ["Pacer"]
+      },
+      {
+        "uid": 70,
+        "name": "임건",
+        "instagram": "2m_7un",
+        "badge": ""
+      },
+      {
+        "uid": 71,
+        "name": "이예담",
+        "instagram": "iysedami",
+        "badge": ""
+      },
+      {
+        "uid": 72,
+        "name": "김상민",
+        "instagram": "p.ksangmin",
+        "badge": ""
+      },
+      {
+        "uid": 73,
+        "name": "김성현",
+        "instagram": "s_hyeon7598",
+        "badge": ""
+      },
+      {
+        "uid": 74,
+        "name": "김진수",
+        "instagram": "jinsoo47",
+        "badge": ""
+      },
+      {
+        "uid": 75,
+        "name": "김승엽",
+        "instagram": "yobbyisfree",
+        "badge": ""
+      },
+      {
+        "uid": 76,
+        "name": "송민경",
+        "instagram": "smingomi",
+        "badge": ""
+      },
+      {
+        "uid": 77,
+        "name": "염태선",
+        "instagram": "",
+        "badge": ""
+      },
+      {
+        "uid": 78,
+        "name": "김민건",
+        "instagram": "minigun00",
+        "badge": ""
+      },
+      {
+        "uid": 79,
+        "name": "김예린",
+        "instagram": "ye_r1nn",
+        "badge": ""
+      },
+      {
+        "uid": 80,
+        "name": "배태진",
+        "instagram": "taejin.0_0",
+        "badge": ""
+      },
+      {
+        "uid": 81,
+        "name": "박채원",
+        "instagram": "won_an_d0y",
+        "badge": ""
+      },
+      {
+        "uid": 82,
+        "name": "김희연",
+        "instagram": "kinghy00",
+        "badge": ""
+      },
+      {
+        "uid": 83,
+        "name": "이채연",
+        "instagram": "c_yxxn__",
+        "badge": ""
+      },
+      {
+        "uid": 84,
+        "name": "김동재",
+        "instagram": "kdzaezae",
+        "badge": ""
+      },
+      {
+        "uid": 85,
+        "name": "윤태린",
+        "instagram": "our1n",
+        "badge": ""
+      },
+      {
+        "uid": 86,
+        "name": "김주연",
+        "instagram": "juyo_omi",
+        "badge": ""
+      },
+      {
+        "uid": 87,
+        "name": "양재혁",
+        "instagram": "_hy32",
+        "badge": ""
+      },
+      {
+        "uid": 88,
+        "name": "박성근",
+        "instagram": "sunggeun8.1",
+        "badge": ""
+      },
+      {
+        "uid": 89,
+        "name": "백은하",
+        "instagram": "baik2thefuture",
+        "badge": ""
+      },
+      {
+        "uid": 90,
+        "name": "김지연",
+        "instagram": "jibongvong",
+        "badge": ""
+      },
+      {
+        "uid": 91,
+        "name": "정민성",
+        "instagram": "minseong250",
+        "badge": ""
+      },
+      {
+        "uid": 92,
+        "name": "송민석",
+        "instagram": "__snobeffect__",
+        "badge": ""
+      },
+      {
+        "uid": 93,
+        "name": "유선우",
+        "instagram": "sw_blissful",
+        "badge": ""
+      },
+      {
+        "uid": 94,
+        "name": "오선빈",
+        "instagram": "sbin_o",
+        "badge": ""
+      },
+      {
+        "uid": 95,
+        "name": "최태림",
+        "instagram": "rimyi_0317",
+        "badge": ""
+      },
+      {
+        "uid": 96,
+        "name": "김민수",
+        "instagram": "minsu_10_14",
+        "badge": ""
+      },
+      {
+        "uid": 97,
+        "name": "안건형",
+        "instagram": "geon_bro0_0",
+        "badge": ""
+      },
+      {
+        "uid": 98,
+        "name": "강수오",
+        "instagram": "gang.suo",
+        "badge": ""
+      },
+      {
+        "uid": 99,
+        "name": "이의제",
+        "instagram": "_euije",
+        "badge": ""
+      },
+      {
+        "uid": 100,
+        "name": "김웅비",
+        "instagram": "",
+        "badge": ""
+      },
+      {
+        "uid": 101,
+        "name": "김혜인",
+        "instagram": "polyhyenylene",
+        "badge": ""
+      },
+      {
+        "uid": 102,
+        "name": "박인우",
+        "instagram": "2.__.nu",
+        "badge": ""
+      },
+      {
+        "uid": 103,
+        "name": "김소현",
+        "instagram": "with_sosohan",
+        "badge": ""
+      },
+      {
+        "uid": 104,
+        "name": "허범현",
+        "instagram": "h_bum_h",
+        "badge": ""
+      }
+] 
